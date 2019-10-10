e29a56ac0423d72233f0f75f1d7b1e9cfc9a96a8
kmskpraveen <bgkmskpraveenmb@gmail.com>
2019-10-10 14:15:53 +0530

    3. Variables and Types

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -7,6 +7,19 @@
   * This is the main object for our application
   */
 object HelloWorld {
+
+  val name = "Pat Cook" // val name:String = "Pat Cook"
+  val value = 42
+  var age = 22
+  age += 1 // when you make a val and points to an object, ..
+  // .. it always points to the same object and not able to reassign
+  // .. but with the help of var we can reassign
+
+  // Some types: String, Int, Double, Char, Boolean, Unit
+  val t = (1,2.7,"hi there") // like Tuple
+  val (a,b,c) = t
+  println(t._1)
+
   /**
     * This is the main method for the application.
     * @param args the arguments to the application.
