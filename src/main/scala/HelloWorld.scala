02f787ac26f5d1b95d41683ed868885b352e12f5
kmskpraveen <bgkmskpraveenmb@gmail.com>
2019-10-10 12:27:04 +0530

    2. Beginning IntelliJ
    Created a basic HelloWorld program

---
+++ src/main/scala/HelloWorld.scala
@@ -0,0 +1,17 @@
+/*
+ * This file created for Object-Orientation, Abstraction,
+ * and Data Structures using Scala
+ */
+
+/**
+  * This is the main object for our application
+  */
+object HelloWorld {
+  /**
+    * This is the main method for the application.
+    * @param args the arguments to the application.
+    */
+  def main(args: Array[String]): Unit = {
+    println("Hello World!") // This prints something
+  }
+}
